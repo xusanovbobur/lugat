1eca1ec7e414c0855bdfdb1c98dc1a97d218ad88
xusanovbobur <140611644+xusanovbobur@users.noreply.github.com>
2024-03-26 22:33:37 +0500

    Add files via upload

---
+++ Unit 4/raum.js
@@ -0,0 +1,222 @@
+function random(param1,param2) {
+  // document.write(`<span style="color: red;"><center>${param1}  </center></span>`);
+  let person = prompt(param1,"translate");
+  if(person==param2){
+    let togri = "TRUE";
+    document.write(`<span style="color: blue;"><center>${togri}  </center></span>`);
+  }else{
+    let xato = "FALSE";
+    document.write(`<span style="color: red; "><center>${xato}  </center></span>`);
+  }
+  //   setTimeout(function() {
+  //   document.write(`<span style="color: red;   "><center>${param1} </center></span>`+`<span style="color: green;   "><center> = </center></span>`+`<span style="color: blue;   "><center>${param2} </center></span>`);
+  // }, 2000);
+}
+
+let a1 = "a lot of";             let b1="ko'p"
+let a2 = "accountant";           let b2="bug'alter"
+let a3 = "age";                  let b3 ="yosh"
+let a4 = "bank manager";         let b4="bank menejeri"
+let a5 = "bank";                 let b5="bank"
+let a6 = "best friend";          let b6="eng yaxshi do'st"
+let a7 = "big";                  let b7="katta"
+let a8 = "both";                 let b8="birgalikda"
+let a9 = "boyfriend";            let b9="o'gil bola do'st"
+let a10 = "business card";       let b10="vizitka"
+let a11= "certainly";            let b11="aniq"
+let a12= "children";             let b12="bolalar"
+let a13= "college";              let b13="kollej"
+let a14= "Come on!";             let b14="tezroq!"
+let a15= "company";              let b15="kompaniya"
+let a16= "connecting";           let b16="ulanmoq"
+let a17= "dancing";              let b17="o'ynamoq"
+let a18= "dictionary";           let b18="lug'at"
+let a19= "dog";                  let b19="it"
+let a20= "evening";              let b20="oqshom"
+let a21= "everybody";            let b21="hamma"
+let a22= "family tree";          let b22="oila shajarasi"
+let a23 = "fans";                let b23="muhlislar"
+let a24 = "foodball";            let b24="futbol"
+let a25 = "friends";             let b25="do'stlar"
+let a26 = "funny";               let b26="kulgili"
+let a27= "Germany";              let b27="Germaniya"
+let a28= "girlfriend";           let b28="qiz do'st"
+let a29= "give";                 let b29="bermoq"
+let a30= "goal";                 let b30="maqsad"
+let a31= "good";                 let b31="yaxshi"
+let a32= "have";                 let b32="bor"
+let a33= "home";                 let b33="uy"
+let a34= "hotel";                let b34="mehmohona"
+let a35= "like";                 let b35="yaxshi ko'rmoq"
+let a36= "manager";              let b36="menejer"
+let a37= "near";                 let b37="yaqin"
+let a38= "nice";                 let b38="yaxshi"
+let a39= "north";                let b39="shimol"
+let a40= "notice";               let b40="sezmoq"
+let a41= "of course";            let b41="albatta"
+let a42= "office";               let b42="offis"
+let a43= "our";                  let b43="bizning"
+let a44= "part of";              let b44="qismi"
+let a45= "part time";            let b45="to'liq bo'lmagan"
+let a46= "police";               let b46="politsiya"
+let a47= "really";               let b47="rosdan"
+let a48 = "small";               let b48="kichik"
+let a49 = "sport";               let b49="sport"
+let a50 = "sport centre";        let b50="sport markazi"
+let a51 = "their";               let b51="ularning"
+let a52 = "together";            let b52="birgalikda"
+let a53 = "univercity";          let b53="universited"
+let a54 = "us";                  let b54="bizga"
+let a55 = "village";             let b55="qishloq"
+let a56 = "want";                let b56="hohlamoq"
+let a57 = "brother";             let b57="ak=uka"
+let a58 = "daughter";            let b58="qiz farzand"
+let a59 = "father";              let b59="ota"
+let a60 = "husband";             let b60="er"
+let a61 = "mother";              let b61="ona"
+let a62 = "parents";             let b62="ota-ona"
+let a63 = "sister";              let b63="singil"
+let a64 = "son";                 let b64="o'g'il farzand"
+let a65 = "spell";               let b65="talaffuz qilmoq"
+let randomVariable = Math.floor(Math.random() * 65);
+
+
+if(randomVariable==1){
+  random(a1,b1)
+}else if(randomVariable==2){
+  random(a2,b2)
+}else if(randomVariable==3){
+  random(a3,b3)
+}else if(randomVariable==4){
+  random(a4,b4)
+}else if(randomVariable==5){
+  random(a5,b5)
+}else if(randomVariable==6){
+  random(a6,b6)
+}else if(randomVariable==7){
+  random(a7,b7)
+}else if(randomVariable==8){
+  random(a8,b8)
+}else if(randomVariable==9){
+  random(a9,b9)
+}else if(randomVariable==10){
+  random(a10,b10)
+}else if(randomVariable==11){
+  random(a11,b11)
+}else if(randomVariable==12){
+  random(a12,b12)
+}else if(randomVariable==13){
+  random(a13,b13)
+}else if(randomVariable==14){
+  random(a14,b14)
+}else if(randomVariable==15){
+  random(a15,b15)
+}else if(randomVariable==16){
+  random(a16,b16)
+}else if(randomVariable==17){
+  random(a17,b17)
+}else if(randomVariable==18){
+  random(a18,b18)
+}else if(randomVariable==19){
+  random(a19,b19)
+}else if(randomVariable==20){
+  random(a20,b20)
+}else if(randomVariable==21){
+  random(a21,b21)
+}else if(randomVariable==22){
+  random(a22,b22)
+}else if(randomVariable==23){
+  random(a23,b23)
+}else if(randomVariable==24){
+  random(a24,b24)
+}else if(randomVariable==25){
+  random(a25,b25)
+}else if(randomVariable==26){
+  random(a26,b26)
+}else if(randomVariable==27){
+  random(a27,b27)
+}else if(randomVariable==28){
+  random(a28,b28)
+}else if(randomVariable==29){
+  random(a29,b29)
+}else if(randomVariable==30){
+  random(a30,b30)
+}else if(randomVariable==31){
+  random(a31,b31)
+}else if(randomVariable==32){
+  random(a32,b32)
+}else if(randomVariable==33){
+  random(a33,b33)
+}else if(randomVariable==34){
+  random(a34,b34)
+}else if(randomVariable==35){
+  random(a35,b35)
+}else if(randomVariable==36){
+  random(a36,b36)
+}else if(randomVariable==37){
+  random(a37,b37)
+}else if(randomVariable==38){
+  random(a38,b38)
+}else if(randomVariable==39){
+  random(a39,b39)
+}else if(randomVariable==40){
+  random(a40,b40)
+}else if(randomVariable==41){
+  random(a41,b41)
+}else if(randomVariable==42){
+  random(a42,b42)
+}else if(randomVariable==43){
+  random(a43,b43)
+}else if(randomVariable==44){
+  random(a44,b44)
+}else if(randomVariable==45){
+  random(a45,b45)
+}else if(randomVariable==46){
+  random(a46,b46)
+}else if(randomVariable==47){
+  random(a47,b47)
+}else if(randomVariable==48){
+  random(a48,b48)
+}else if(randomVariable==49){
+  random(a49,b49)
+}else if(randomVariable==50){
+  random(a50,b50)
+}else if(randomVariable==51){
+  random(a51,b51)
+}else if(randomVariable==52){
+  random(a52,b52)
+}else if(randomVariable==53){
+  random(a53,b53)
+}else if(randomVariable==54){
+  random(a54,b54)
+}else if(randomVariable==55){
+  random(a55,b55)
+}else if(randomVariable==56){
+  random(a56,b56)
+}else if(randomVariable==57){
+  random(a57,b57)
+}else if(randomVariable==58){
+  random(a58,b58)
+}else if(randomVariable==59){
+  random(a59,b59)
+}else if(randomVariable==60){
+  random(a60,b60)
+}else if(randomVariable==61){
+  random(a61,b61)
+}else if(randomVariable==62){
+  random(a62,b62)
+}else if(randomVariable==63){
+  random(a63,b63)
+}else if(randomVariable==64){
+  random(a64,b64)
+}else if(randomVariable==65){
+  random(a65,b65)
+}else {
+document.write(`<span style="color: red;   "><center>wife =</center></span>`);
+    setTimeout(function() {
+    document.write(`<span style="color: red;   "><center>wife </center></span>`+`<span style="color: green;   "><center> = </center></span>`+`<span style="color: blue;   "><center>xotin</center></span>`);
+  }, 2000);
+}
+setTimeout(function() {
+      location.reload();
+    }, 5000);
---
+++ Unit 4/tajriba/raum.js
@@ -0,0 +1,6 @@
+let person = prompt("School", "translate");
+if(person=="Maktab"||person=="maktab"){
+    document.write("SUCCSES");
+}else{
+    document.write("failed")
+}
